729c2cf79e04d96fa5c36dd24ae3ad113bd972ea
Sebastian Bentancurt <sbentancurt@netsuite.com>
2022-07-22 01:41:54 -0400

    Adding All

---
+++ src/build/precache-manifest.8928eb52fb991b214f14cede47da26ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "82b5826dcd1b48a6de80",
+    "url": "/static/css/main.fe770221.chunk.css"
+  },
+  {
+    "revision": "82b5826dcd1b48a6de80",
+    "url": "/static/js/main.df788b84.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca1d3e7a8e950e694544",
+    "url": "/static/js/2.61abe73b.chunk.js"
+  },
+  {
+    "revision": "8087eb0dad9f72ef1d7de6d484b98e8a",
+    "url": "/index.html"
+  }
+];
---
+++ src/server.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import bodyParser  from 'body-parser'
+import { MongoClient} from 'mongodb'
+import path from 'path'
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')))
+app.use(bodyParser.json());
+const withDB = async(operations, res) => { 
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+        const db = client.db('my-blog');
+        await operations(db)
+        client.close();
+    } catch (error) { 
+        console.log(error)
+        res.status(500).json({message: 'Error connectinbg to db', error});
+    }
+}
+app.get('/api/articles/:name', async (req, res) => { 
+    withDB(async (db) => { 
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('article').findOne({ name: articleName });
+        res.status(200).json(articleInfo);
+    },res)
+})
+app.post('/api/articles/:name/add-comment', (req, res) => { 
+    const { userName, text } = req.body;
+    const articleName = req.params.name;
+    withDB(async (db) => {
+        const articleInfo = await db.collection('article').findOne({ name: articleName });
+
+        await db.collection('article').updateOne({
+            name: articleName
+        }, {
+            '$set': {
+                comments: articleInfo.comments.concat({userName,text})
+            }
+        })
+        const updateArticleInfo = await db.collection('article').findOne({ name: articleName });
+        res.status(200).json(updateArticleInfo)
+    }, res)
+    
+
+})
+app.post('/api/articles/:name/upvote', async (req, res) => {
+
+    withDB(async (db) => { 
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('article').findOne({ name: articleName });
+        await db.collection('article').updateOne({ name: articleName },
+            {
+                '$set': {
+                    upvotes:articleInfo.upvotes + 1
+                }
+            })
+        const updateArticleInfo = await db.collection('article').findOne({ name: articleName });
+        res.status(200).json(updateArticleInfo);
+    },res)
+});
+
+app.get('*', (req, res) => { 
+    res.sendfile(path.join(__dirname + '/build/index.html'))
+})
+app.listen(8000, () => console.log('Listening on port 8000'));
+
